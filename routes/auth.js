d6d2e06d46ae91c886071b6148d25471003c36d7
Himanshu25102005 <himanshudusane12@gmail.com>
2025-10-22 15:37:34 +0530

    authentication-2

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/api/google/callback",
+    callbackURL: "https://backendv2-for-dep-production.up.railway.app/api/google/callback",
     scope: ['profile', 'email']
   },
   async function(accessToken, refreshToken, profile, cb) {
